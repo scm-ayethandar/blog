a4eb14b47ccb41d619247d639ed4d4e56f496f39
Aye Thandar <thandarthandar549@gmail.com>
2022-06-12 11:52:15 +0630

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/store', [PostController::class, 'store']);
+
+Route::get('/posts/edit/{id}', [PostController::class, 'edit']);
+Route::post('/posts/update/{id}', [PostController::class, 'update']);
+
+Route::get('/posts/show/{id}', [PostController::class, 'show']);
+Route::get('/posts/delete/{id}', [PostController::class, 'destroy']);
